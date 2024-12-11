03c8fcedd178ce94d8356a85f2909b2e825c44fd
Satria <satriasauraly@gmail.com>
2024-12-11 10:19:23 +0700

    Fix Docker File

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM python:3.10
-
 WORKDIR /app
-COPY . /app
-RUN pip install --upgrade pip
+COPY requirements.txt /app/ 
+RUN pip install --upgrade pip 
 RUN pip install -r requirements.txt
-
+COPY . /app
 EXPOSE 8080
-CMD ["python", "./main.py" ]
+CMD ["python", "main.py" ]
--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def predicting_input(mytext):
 def index():
     text = request.form['text']
     if text is None:
-        return jsonify({"error": "where is your file broh"})
+        return jsonify({"error": "Form data not found"})
     
     predict_result = predicting_input(text)
     
@@ -93,7 +93,3 @@ def index():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
     app.run(debug=True, host='0.0.0.0', port=port)
-
-
-
-
